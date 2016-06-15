c13fea9a61a5917e100497bd51ac347b13e95809
Laura Harumi <laura.vazquez.fukazawa@gmail.com>
2016-06-15 18:59:23 +0900

    return decimal value when divided

--- hmk1-calculator.py
+++ hmk1-calculator.py
@@ -13,7 +13,6 @@ def readNumber(line, index):
     token = {'type': 'NUMBER', 'number': number}
     return token, index
 
-
 def readPlus(line, index):
     token = {'type': 'PLUS'}
     return token, index + 1
@@ -50,12 +49,15 @@ def tokenize(line):
         tokens.append(token) #appending eg {'type': 'MINUS'}
     return tokens
 
+
+################### added work ###################################
+
 def solveMult(tokens, newtokens, index): 
     solution = {'type': 'NUMBER', 'number': newtokens[-2]['number'] * tokens[index]['number']}
     return solution, index + 1 
 
 def solveDiv(tokens, newtokens, index): 
-    solution = {'type': 'NUMBER', 'number': newtokens[-2]['number'] / tokens[index]['number']}
+    solution = {'type': 'NUMBER', 'number': newtokens[-2]['number'] / float(tokens[index]['number'])} #float so that 1/5 gives 0.2 not 0 
     return solution, index + 1 
 
 def ignore(tokens, index): 
@@ -69,14 +71,18 @@ def firstsolve(tokens): #just solve multiplications and divisions, ignore the re
         if tokens[index-1]['type'] == "MUL": #eg if 8*2, index is in 2 
             (solution, index) = solveMult(tokens, newtokens, index) #append 8 
             newtokens = newtokens[:-2] #remove 8*
-
         elif tokens[index-1]['type'] == "DIV":
             (solution, index) = solveDiv(tokens, newtokens, index)
             newtokens = newtokens[:-2]
         elif tokens[index-1]['type'] in "NUMBERPLUSMINUS":  
             (solution, index) = ignore(tokens, index)
+        else:
+            print 'Invalid syntax'
         newtokens.append(solution)
     return newtokens 
+
+#################### end of added work ############################
+
 
 def evaluate(newtokens): #final solve - solve the remaining additions and subtractions
     answer = 0
@@ -86,10 +92,8 @@ def evaluate(newtokens): #final solve - solve the remaining additions and subtra
         if newtokens[index]['type'] == 'NUMBER':
             if newtokens[index - 1]['type'] == 'PLUS':
                 answer += newtokens[index]['number'] 
-            elif newtokens[index - 1]['type'] == 'MINUS':
+            else: # if newtokens[index - 1]['type'] == 'MINUS':
                 answer -= newtokens[index]['number']
-            else:
-                print 'Invalid syntax'
         index += 1
     return answer
 
@@ -98,5 +102,7 @@ while True:
     line = raw_input().replace(' ','') #remove spaces 
     tokens = tokenize(line) #tokenize everything 
     newtokens= firstsolve(tokens) #new tokens with mutl and div solved, only + and - remaining
+    print "tokens: " + ''.join(str([pairs.values()[len(pairs)-1] for pairs in tokens])) #printing just the values
+    print "simplified tokens: " + ''.join(str([pairs.values()[len(pairs)-1] for pairs in newtokens]))
     answer = evaluate(newtokens) # solve plus and minuses 
     print "answer = %f\n" % answer 
